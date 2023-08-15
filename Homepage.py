6d52af99026cc1a8fc72eb036f74d9b358a07062
Philipe-Micioneiro <134943051+Philipe-Micioneiro@users.noreply.github.com>
2023-08-15 18:10:59 -0300

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Home' )
+
+st.title('Projetos de ML - Philipe Micioneiro')
+st.subheader('Por favor, fique a vontade para conhecer meus projetos ')
+st.sidebar.success("Escolha qual deseja ver primeiro")
+
