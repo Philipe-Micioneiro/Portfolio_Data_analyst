79edc0906a290875f04d166aa82e1ba5d83c625f
Philipe-Micioneiro <134943051+Philipe-Micioneiro@users.noreply.github.com>
2023-08-16 12:25:13 -0300

    Update Calculo de Plano de Saúde.py

--- multipage_app/pages/Calculo de Plano de Saúde.py
+++ multipage_app/pages/Calculo de Plano de Saúde.py
@@ -7,6 +7,11 @@ from sklearn.linear_model import LinearRegression
 import streamlit as st
 
 #IMPORTANTE : RODAR O CODIGO :  streamlit run Streamlit_Regressao_Linear_simples_Plano_Saude.py
+st.set_page_config(page_title= Plano de Saúde')
+st.write('Projeto : Calculo de contratação de planos de saúde')
+st.write('Objetivo : Validar que o valor cobrado é justo, bem como saber onde estariamos ganhando a mais, ou perdendo demais. O algoritmo também tem o poder de saber valores futuros com a regressão.')
+st.write('Algoritmo utilizado: Linear Regression')
+st.write('precisão e assertividade do modelo: 93,8%')
 
 #titulo
 st.write('Plano de Saúde')
