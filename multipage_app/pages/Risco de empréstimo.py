d82c9ba410969e0f3141a97e221e9b67a900a37e
Philipe-Micioneiro <134943051+Philipe-Micioneiro@users.noreply.github.com>
2023-08-16 12:12:16 -0300

    Update Risco de empréstimo.py

--- multipage_app/pages/Risco de empréstimo.py
+++ multipage_app/pages/Risco de empréstimo.py
@@ -4,8 +4,8 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
 
-# Projeto : Risco de Emprestimo
-#Objetivo : Por caracteristicas do solicitante, descobrir se a taxa de risco de emprestimo é alta ou baixa.
+st.write('Projeto : Risco de Emprestimo')
+st.write('Objetivo : Por caracteristicas do solicitante, descobrir se a taxa de risco de emprestimo é alta ou baixa.')
 #ALGORITMO : NAIVE BAYES
 #accuracy do modelo: 96,3%
 
